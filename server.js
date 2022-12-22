5226c3ee996c786cc19412b1acd63b43fee20e88
lmartin214@yahoo.com <you@example.com>
2022-12-22 12:44:25 -0800

    setup handlebars engine and forced sycned DB

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const path = require('path');
+// Dependencies
 const express = require('express');
 const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
 
+// Set up the Express App
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
-// Incorporate the custom helper methods
-const hbs = exphbs.create({ helpers });
-
+// Setting Handlebars.js as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
