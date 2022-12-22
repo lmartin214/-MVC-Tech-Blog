e1e54bc9d1d4c3add2a368ccd904503b538c4176
lmartin214@yahoo.com <you@example.com>
2022-12-22 13:43:48 -0800

    added express-session np for storing user data, added Post model

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-// Dependencies
+//Dependencies
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const hbs = exphbs.create({});
 
-// Set up the Express App
+//Set up the Express App
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-// Setting Handlebars.js as the default template engine.
+const sess = {
+  secret: 'Keep it on the low',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
+// Setting Handlebars.js as the template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
