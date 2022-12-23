3c10814eafcdde7debea9b698a9a66d8920735c4
lmartin214@yahoo.com <you@example.com>
2022-12-22 17:20:20 -0800

    route for user Posts and Comments

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+
+//Creates a new comment
+router.get("/", async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//Deletes an existing comment
+router.delete("/:id", async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: "Comment not found" });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
